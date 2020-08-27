6c8233c08ddbac1b51625a8cb41958002491dfef
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-08-26 23:30:06 -0700

    add element divs to QuickMatch

--- src/components/QuickMatch.js
+++ src/components/QuickMatch.js
@@ -6,6 +6,23 @@ const QuickMatch = () => {
     <>
       <h1>Quick Match</h1>
 
+      <h2>Choose an element</h2>
+
+      <div className="elements">
+        <div className="fire">
+
+        </div>
+
+        <div className="water">
+
+        </div>
+
+        <div className="plant">
+
+        </div>
+
+      </div>
+
     </>
   )
 
